a8410e83638a1d822d83aff5102e2b2685ec8baa
maliaoreta <maliaoreta@gmail.com>
2016-03-22 10:47:22 -1000

    removes punctuations and translates piglatin back to english

--- pigLatin.js
+++ pigLatin.js
@@ -1,18 +1,16 @@
 var pigLatinTranslator = function (phrase) {
 
   var pigString = '';
+  phrase = phrase.replace(/[\d\.,?!'@$#%^*"()/\\]/g, '');
   phrase = phrase.toLowerCase().split(' ');
+  
   phrase.forEach(function (word) {
 
     pigString = pigString.concat(pigLatinWordTranslator(word) + ' ');
   })
 
-
-  console.log('pigString: ', pigString);
+  return pigString;
 }
-
-
-
 
 
 var pigLatinWordTranslator = function (word) {
@@ -32,16 +30,43 @@ var pigLatinWordTranslator = function (word) {
   }
 
   var removedCons = word.splice(0, firstVowel);
-  removedCons = removedCons.join('');
-  
-  
+  removedCons = removedCons.join('');  
   word = word.join('').concat('-' + removedCons + ay);
-  
   return word;
 }
 
+console.log(pigLatinTranslator('Th/?is 4p3hra.se will. be. convert.,e6d into piglatin wit889out sy3mb.ols'));
 
 
+var translatePhraseToEnglish = function (phrase) {
 
+  var english = '';
+  phrase = phrase.replace(/[\d\.,?!'@$#%^*"()/\\]/g, '');
+  phrase = phrase.toLowerCase().split(' ');
 
-pigLatinTranslator('This phrase will be converted into piglatin');
+  phrase.forEach(function (word) {
+
+    english = english.concat(translateWordToEnglish(word) + ' ');
+  })
+  
+  return english;
+}
+
+var translateWordToEnglish = function (word) {
+
+  var ayIndex = 0;
+
+  if (word.indexOf('-') !== -1) {
+
+    ayIndex = word.indexOf('-');
+  }
+
+  var replaceCons = word.slice(ayIndex);
+  replaceCons = replaceCons.replace('-', '');
+  replaceCons = replaceCons.replace('ay', '');
+
+  var pigWord = replaceCons + word.slice(0, ayIndex);
+  return pigWord;
+}
+
+console.log(translatePhraseToEnglish('is-th...ay56) i,s-ay iglatin0,/-pay'));
